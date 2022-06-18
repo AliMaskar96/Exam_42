9dd0c11a55b29c3cd6fc9a7ff6d81928469c3b0b
Ali Ait maskar <aait-mas@e1r6p7.1337.ma>
2022-06-18 04:41:10 +0100

    Pool

---
+++ Level01/fizzbuzz.c
@@ -0,0 +1,37 @@
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putnbr(int nb)
+{
+	if (nb >= 10)
+	{
+		ft_putnbr(nb / 10);
+		ft_putnbr(nb % 10);
+	}
+	else
+		ft_putchar(nb + '0');
+}
+
+int	main(void)
+{
+	int	number;
+
+	number = 1;
+	while (number <= 100)
+	{
+		if (number % 3 == 0 && number % 5 == 0)
+			write(1, "fizzbuzz", 8);
+		else if (number % 3 == 0)
+			write(1, "fizz", 4);
+		else if (number % 5 == 0)
+			write(1, "buzz", 4);
+		else
+			ft_putnbr(number);
+		write(1, "\n", 1);
+		number++;
+	}
+}
---
+++ Level01/repeat_alpha.c
@@ -0,0 +1,34 @@
+#include <unistd.h>
+
+int		main(int ac, char **av)
+{
+	int i;
+	int count;
+
+	i = 0;
+	count = 0;
+	if (ac == 2)
+	{
+		while (av[1][i])
+		{
+			if (av[1][i] >= 'A' && av[1][i] <= 'Z')
+				count = av[1][i] - 64;
+			else if (av[1][i] >= 'a' && av[1][i] <= 'z')
+				count = av[1][i] - 96;
+			else
+			{
+				write(1, &av[1][i], 1);
+				count = 0;
+			}
+			while (count)
+			{
+				write(1, &av[1][i], 1);
+				count--;
+			}
+			count = 1;
+			i++;
+		}
+	}
+	write(1, "\n", 1);
+	return (0);
+}
